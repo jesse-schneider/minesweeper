f64f7b174b56e3d4f6b2d09de66fc9115cbf75e6
Jesse Schneider <jessewschneider96@gmail.com>
2019-07-28 17:27:45 +1000

    changed GUI library slightly - went from PySide2 to Qt5, implemented board initially

--- minesweeper.py
+++ minesweeper.py
@@ -1,69 +1,81 @@
 import sys
 import random
-from PySide2 import QtCore, QtWidgets, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-class mainWindow(QtWidgets.QWidget):
+
+class mainWindow(QWidget):
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
 
-        self.button = QtWidgets.QPushButton("Start!")
-        self.grid = {}
-        self.text = QtWidgets.QLabel("Welcome to 10x10 Minesweeper!")
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
+        self.board_size = 10
+        self.num_mines = 10
+        self.status = "Playing"
 
-        self.layout = QtWidgets.QGridLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-        self.layout.setSpacing(0)
-        self.layout.setVerticalSpacing(0)
+        w = QWidget()
+        hb = QHBoxLayout()
 
-        self.button.clicked.connect(self.startGame)
+        self.mines = QLabel()
+        self.mines.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+        self.clock = QLabel()
+        self.clock.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+        f = self.mines.font()
+        f.setPointSize(24)
+        f.setWeight(75)
+        self.mines.setFont(f)
+        self.clock.setFont(f)
+
+        self._timer = QTimer()
+        self._timer.timeout.connect(self.update_timer)
+        self._timer.start(1000)  # 1 second timer
+
+        self.mines.setText("%02d" % self.num_mines)
+        self.clock.setText("000")
+
+        self.button = QPushButton("Reset")
+        self.button.setFixedSize(QSize(70, 50))
+
+        self.button.pressed.connect(self.button_reset)
+
+        hb.addWidget(self.mines)
+        hb.addWidget(self.button)
+        hb.addWidget(self.clock)
+
+        vb = QVBoxLayout()
+        vb.addLayout(hb)
+
+        self.grid = QGridLayout()
+        self.grid.setSpacing(5)
+
+        vb.addLayout(self.grid)
+        w.setLayout(vb)
+        self.setCentralWidget(w)
+
+        self.create_board()
+
+        self.reset_map()
+        self.show()
+
+    def create_board(self):
+        # Add positions to the board
+        for i in range(0, self.board_size):
+            for j in range(0, self.board_size):
+                tile = Tile(i, j)
+                self.grid.addWidget(tile, i, j)
+                # Connect signals to cell for starting game, expanding area and ending game 
+                tile.clicked.connect(self.start_game)
+                tile.expand.connect(self.expand_reveal_area)
+                tile.clicked_mine.connect(self.game_over)
 
 
-    def startGame(self):
-        self.layout.removeWidget(self.button)
-        self.layout.removeWidget(self.text)
-        for i in range(10):
-            for j in range(10):
-                self.grid[(i, j)] = QtWidgets.QPushButton("%d %d" % (i, j))
-                self.grid[(i, j)].setFixedSize(50,50)
-                self.grid[(i, j)].clicked.connect(self.checkMine)
-                self.layout.addWidget(self.grid[(i, j)], i, j)
-
-
-
-    def checkMine(self):
-        print("checking for mine")
-
-
-
-class mineBoard():
-    def __init__(self):
-        self.numberMines = 10
-        self.board = []
-
-
-    def createBoard(self):
-        #initialise the game board
-        for i in range(10):
-            for j in range(10):
-                self.board.append('%d%d' % (i, j)) 
-        #add mines to the game board
-        for r in range(10):
-            newMine = random.randint(0, 99)
-            self.board[newMine] = 'M'
 
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-
-    widget = mainWindow()
-    widget.resize(500,500)
-    widget.show()
-
-    board = mineBoard()
-    board.createBoard()
+    app = QApplication([])
+    window = MainWindow()
 
     sys.exit(app.exec_())
